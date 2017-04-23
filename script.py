b12236ba7b57e6a12806597bf33b2c53f21de7be
alireza <nazarihome@gmail.com>
2017-04-23 18:13:50 -0400

    scripts2

--- script.py
+++ script.py
@@ -1,6 +1,2 @@
-import plumbum
-r = plumbum.machines.RemoteMachine("104.198.152.153",
- user="nazarihome", password="mohMOH13579")
-fro = plumbum.local.path("README.md")
-to = r.path("~/README.md")
-plumbum.path.utils.copy(fro, to)
+import os 
+import paramiko
