b214520694b2f7a3a59258780b0ce3111c06e7b3
alireza <nazarihome@gmail.com>
2017-04-23 17:48:01 -0400

    scripts2

--- script.py
+++ script.py
@@ -1,17 +1,6 @@
-from paramiko_scp
-
-#client = scp.Client(host="104.198.152.153", user="nazarihome", password="mohMOH13579")
-
-# and then
-#client.transfer('README.md', '~/README.md')
-
-
-def createSSHClient(server, port, user, password):
-    client = paramiko.SSHClient()
-    client.load_system_host_keys()
-    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(server, port, user, password)
-    return client
-
-ssh = createSSHClient(host="104.198.152.153", user="nazarihome", password="mohMOH13579")
-scp = SCPClient(ssh.get_transport())
+import plumbum
+r = plumbum.machines.RemoteMachine("104.198.152.153",
+ user="nazarihome", password="mohMOH13579")
+fro = plumbum.local.path("README.md")
+to = r.path("~/README.md")
+plumbum.path.utils.copy(fro, to)
