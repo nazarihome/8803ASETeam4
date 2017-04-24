633dab08aca1900578ace294d32a3d84e1d8a388
alireza <nazarihome@gmail.com>
2017-04-24 11:32:10 -0400

    final16

--- ServerTester/validate.py
+++ ServerTester/validate.py
@@ -1,7 +1,7 @@
 import sys
 if 'Failures: 1' in open('out.txt').read():
-	print("1 Test Failed")
-    sys.exit(1)
+   print('1 Test Failed')
+   sys.exit(1)
 if 'Failures: 2' in open('out.txt').read():
-	print("2 Tests Failed")
-    sys.exit(1)
+   print('2 Tests Failed"')
+   sys.exit(1)
